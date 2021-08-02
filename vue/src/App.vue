ec74ade7de7ab9c229ba205cf2105965d5b7f8cd
caryn corey <caryn.corey@gmail.com>
2021-08-02 15:35:29 -0400

    added veutify and testing usage on username/password view components

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,9 +1,9 @@
 <template>
-  <div id="app">
+  <v-app id="app">
     <div id="nav">
       <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
       <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
     </div>
     <router-view />
-  </div>
+  </v-app>
 </template>
