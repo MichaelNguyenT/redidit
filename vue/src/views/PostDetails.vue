213b27acb48245b953b7473118f2fb98ee1908a9
greg lowery <gregory.lowery20@gmail.com>
2021-08-02 16:33:02 -0400

    sample post component and router created

---
+++ vue/src/views/PostDetails.vue
@@ -0,0 +1,19 @@
+<template>
+    <div><post-detail /></div>
+</template>
+
+<script>
+
+import PostDetail from "../components/PostDetail.vue"
+export default {
+    name: "post-details",
+    components: {
+        PostDetail
+    }
+
+}
+</script>
+
+<style>
+
+</style>
