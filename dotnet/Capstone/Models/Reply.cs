f713b83ef5ebfb31e433ffddaa6e2e7d277e583b
Shane Weingartner <weingartners8324@gmail.com>
2021-08-02 12:45:49 -0400

    First DAO Commit

---
+++ dotnet/Capstone/Models/Reply.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Capstone.Models
+{
+    public class Reply
+    {
+        public int ReplyId { get; set; }
+        public int PostId { get; set; }
+        public string Username { get; set; }
+        public string Content { get; set; }
+        public DateTime PostedDate { get; set; }
+    }
+}
