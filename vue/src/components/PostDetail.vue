857b95c5ed27415247e64539b1818cb339de1420
caryn corey <caryn.corey@gmail.com>
2021-08-03 09:10:04 -0400

    templated out possible post detail card

--- vue/src/components/PostDetail.vue
+++ vue/src/components/PostDetail.vue
@@ -1,6 +1,36 @@
 <template>
     <div> 
-        
+        <v-container>
+        <v-row no-gutters align="center">
+        <v-card elevation="3" outlined shaped>
+            <v-card-title>Title of Post</v-card-title>
+            <v-card-subtitle>Author of Post</v-card-subtitle>
+            <v-card-subtitle>Date/Time</v-card-subtitle>
+            <v-divider class="mt-0 mx-4"></v-divider>
+            <v-card-text>Maecenas eu diam facilisis, tincidunt elit at, laoreet dolor. Proin ultrices, ligula quis pellentesque pharetra, lorem est bibendum erat, in fermentum velit purus vel leo. Ut commodo ex vitae scelerisque laoreet. Curabitur nec viverra orci. Sed tempor lorem nec nisi dapibus, id tincidunt urna efficitur. Cras sagittis eros mollis mi faucibus, a accumsan elit rutrum. Donec vitae nibh posuere, condimentum turpis accumsan, posuere eros. Pellentesque faucibus, augue a tincidunt aliquet, tellus tellus interdum purus, ac rhoncus lectus diam eu purus. Praesent aliquam diam eu est varius, a hendrerit mauris laoreet. Aliquam pharetra eros nisl, sed pretium lorem sollicitudin id.Maecenas eu diam facilisis, tincidunt elit at, laoreet dolor. Proin ultrices, ligula quis pellentesque pharetra, lorem est bibendum erat, in fermentum velit purus vel leo. Ut commodo ex vitae scelerisque laoreet. Curabitur nec viverra orci. Sed tempor lorem nec nisi dapibus, id tincidunt urna efficitur. Cras sagittis eros mollis mi faucibus, a accumsan elit rutrum. Donec vitae nibh posuere, condimentum turpis accumsan, posuere eros. Pellentesque faucibus, augue a tincidunt aliquet, tellus tellus interdum purus, ac rhoncus lectus diam eu purus. Praesent aliquam diam eu est varius, a hendrerit mauris laoreet. Aliquam pharetra eros nisl, sed pretium lorem sollicitudin id.</v-card-text>
+            <v-divider class="mt-0 mx-4"></v-divider>
+            <v-col align-self="end">
+            <v-chip>Yes
+                 <v-icon medium>
+                     mdi-duck
+                </v-icon>
+            </v-chip>
+            <v-chip>Eww
+                 <v-icon medium>
+                     mdi-beehive-off-outline
+                </v-icon>
+            </v-chip>
+            </v-col>
+        </v-card>
+        <v-spacer></v-spacer>
+        <v-card elevation="3" outlined shaped>
+            <v-card-subtitle>Author of Post</v-card-subtitle>
+            <v-card-subtitle>Date/Time</v-card-subtitle>
+            <v-divider class="mt-0 mx-4"></v-divider>
+            <v-card-text>Maecenas eu diam facilisis, tincidunt elit at, laoreet dolor. Proin ultrices, ligula quis pellentesque pharetra, lorem est bibendum erat, in fermentum velit purus vel leo. Ut commodo ex vitae scelerisque laoreet. Curabitur nec viverra orci. Sed tempor lorem nec nisi dapibus, id tincidunt urna efficitur. Cras sagittis eros mollis mi faucibus, a accumsan elit rutrum. Donec vitae nibh posuere, condimentum turpis accumsan, posuere eros. Pellentesque faucibus, augue a tincidunt aliquet, tellus tellus interdum purus, ac rhoncus lectus diam eu purus. Praesent aliquam diam eu est varius, a hendrerit mauris laoreet. Aliquam pharetra eros nisl, sed pretium lorem sollicitudin id.Maecenas eu diam facilisis, tincidunt elit at, laoreet dolor. Proin ultrices, ligula quis pellentesque pharetra, lorem est bibendum erat, in fermentum velit purus vel leo. Ut commodo ex vitae scelerisque laoreet. Curabitur nec viverra orci. Sed tempor lorem nec nisi dapibus, id tincidunt urna efficitur. Cras sagittis eros mollis mi faucibus, a accumsan elit rutrum. Donec vitae nibh posuere, condimentum turpis accumsan, posuere eros. Pellentesque faucibus, augue a tincidunt aliquet, tellus tellus interdum purus, ac rhoncus lectus diam eu purus. Praesent aliquam diam eu est varius, a hendrerit mauris laoreet. Aliquam pharetra eros nisl, sed pretium lorem sollicitudin id.</v-card-text>
+        </v-card>
+        </v-row>
+       </v-container>
     </div>
 </template>
 
