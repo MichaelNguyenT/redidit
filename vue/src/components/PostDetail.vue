5dd3305f866d23a971b7f6b721d8d9cf398df5d4
caryn corey <caryn.corey@gmail.com>
2021-08-03 10:41:02 -0400

    more work on post page formatting

--- vue/src/components/PostDetail.vue
+++ vue/src/components/PostDetail.vue
@@ -9,7 +9,7 @@
             <v-divider class="mt-0 mx-4"></v-divider>
             <v-card-text>Maecenas eu diam facilisis, tincidunt elit at, laoreet dolor. Proin ultrices, ligula quis pellentesque pharetra, lorem est bibendum erat, in fermentum velit purus vel leo. Ut commodo ex vitae scelerisque laoreet. Curabitur nec viverra orci. Sed tempor lorem nec nisi dapibus, id tincidunt urna efficitur. Cras sagittis eros mollis mi faucibus, a accumsan elit rutrum. Donec vitae nibh posuere, condimentum turpis accumsan, posuere eros. Pellentesque faucibus, augue a tincidunt aliquet, tellus tellus interdum purus, ac rhoncus lectus diam eu purus. Praesent aliquam diam eu est varius, a hendrerit mauris laoreet. Aliquam pharetra eros nisl, sed pretium lorem sollicitudin id.Maecenas eu diam facilisis, tincidunt elit at, laoreet dolor. Proin ultrices, ligula quis pellentesque pharetra, lorem est bibendum erat, in fermentum velit purus vel leo. Ut commodo ex vitae scelerisque laoreet. Curabitur nec viverra orci. Sed tempor lorem nec nisi dapibus, id tincidunt urna efficitur. Cras sagittis eros mollis mi faucibus, a accumsan elit rutrum. Donec vitae nibh posuere, condimentum turpis accumsan, posuere eros. Pellentesque faucibus, augue a tincidunt aliquet, tellus tellus interdum purus, ac rhoncus lectus diam eu purus. Praesent aliquam diam eu est varius, a hendrerit mauris laoreet. Aliquam pharetra eros nisl, sed pretium lorem sollicitudin id.</v-card-text>
             <v-divider class="mt-0 mx-4"></v-divider>
-            <v-col align-self="end">
+            <v-col class="d-flex justify-end mb-6">
             <v-chip>Yes
                  <v-icon medium>
                      mdi-duck
@@ -22,10 +22,13 @@
             </v-chip>
             </v-col>
         </v-card>
+        <v-subheader>Replies to this Post...</v-subheader>
         <v-spacer></v-spacer>
         <v-card elevation="3" outlined shaped>
+            <v-col class="d-flex justify-start mb-0">
             <v-card-subtitle>Author of Post</v-card-subtitle>
             <v-card-subtitle>Date/Time</v-card-subtitle>
+            </v-col>
             <v-divider class="mt-0 mx-4"></v-divider>
             <v-card-text>Maecenas eu diam facilisis, tincidunt elit at, laoreet dolor. Proin ultrices, ligula quis pellentesque pharetra, lorem est bibendum erat, in fermentum velit purus vel leo. Ut commodo ex vitae scelerisque laoreet. Curabitur nec viverra orci. Sed tempor lorem nec nisi dapibus, id tincidunt urna efficitur. Cras sagittis eros mollis mi faucibus, a accumsan elit rutrum. Donec vitae nibh posuere, condimentum turpis accumsan, posuere eros. Pellentesque faucibus, augue a tincidunt aliquet, tellus tellus interdum purus, ac rhoncus lectus diam eu purus. Praesent aliquam diam eu est varius, a hendrerit mauris laoreet. Aliquam pharetra eros nisl, sed pretium lorem sollicitudin id.Maecenas eu diam facilisis, tincidunt elit at, laoreet dolor. Proin ultrices, ligula quis pellentesque pharetra, lorem est bibendum erat, in fermentum velit purus vel leo. Ut commodo ex vitae scelerisque laoreet. Curabitur nec viverra orci. Sed tempor lorem nec nisi dapibus, id tincidunt urna efficitur. Cras sagittis eros mollis mi faucibus, a accumsan elit rutrum. Donec vitae nibh posuere, condimentum turpis accumsan, posuere eros. Pellentesque faucibus, augue a tincidunt aliquet, tellus tellus interdum purus, ac rhoncus lectus diam eu purus. Praesent aliquam diam eu est varius, a hendrerit mauris laoreet. Aliquam pharetra eros nisl, sed pretium lorem sollicitudin id.</v-card-text>
         </v-card>
--- vue/src/main.js
+++ vue/src/main.js
@@ -5,6 +5,7 @@ import store from './store/index'
 import axios from 'axios'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
+import colors from 'vuetify/lib/util/colors'
 
 Vue.use(Vuetify)
 
@@ -18,3 +19,16 @@ new Vue({
   vuetify : new Vuetify(),
   render: h => h(App)
 }).$mount('#app')
+
+export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: colors.purple,
+        secondary: colors.grey.darken1,
+        accent: colors.shades.black,
+        error: colors.red.accent3,
+      },
+      },
+    },
+})
