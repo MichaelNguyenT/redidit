daddfbfced2621514040d9b7105b698793a133f8
greg lowery <gregory.lowery20@gmail.com>
2021-08-06 12:12:33 -0400

    color theme functional! and assigned theme colours

--- vue/src/main.js
+++ vue/src/main.js
@@ -5,7 +5,7 @@ import store from './store/index'
 import axios from 'axios'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
-import colors from 'vuetify/lib/util/colors'
+
 
 Vue.use(Vuetify)
 
@@ -13,22 +13,31 @@ Vue.config.productionTip = false
 
 axios.defaults.baseURL = process.env.VUE_APP_REMOTE_API;
 
+const opts = {
+  theme: {
+    dark: false,
+    themes: {
+      light: {
+        primary: '#d300c7',
+        secondary: '#18FFFF',
+        accent: '#e70e5b',
+        error: '#e32828',
+      },
+      dark: {
+        primary: '#18FFFF',
+        secondary: '#d300c7',
+        accent: '#e70e5b',
+        error: '#e32828',
+      },
+    },
+  }
+}
+Vue.config.productionTip = false
+Vue.use(Vuetify)
 new Vue({
   router,
   store,
-  vuetify : new Vuetify(),
-  render: h => h(App)
+  vuetify : new Vuetify(opts),
+  render: h => h(App),
 }).$mount('#app')
 
-export default new Vuetify({
-  theme: {
-    themes: {
-      light: {
-        primary: colors.purple,
-        secondary: colors.grey.darken1,
-        accent: colors.shades.black,
-        error: colors.red.accent3,
-      },
-      },
-    },
-})
--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
+    <span>Home</span>
     <p>You must be authenticated to see this</p>
    
   </div>
