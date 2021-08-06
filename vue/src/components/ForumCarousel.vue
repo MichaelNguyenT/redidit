7f4f7cea40fe0e9b0c14555375ed464b11a0b159
caryn corey <caryn.corey@gmail.com>
2021-08-06 16:27:54 -0400

    final touches on carousel

--- vue/src/components/ForumCarousel.vue
+++ vue/src/components/ForumCarousel.vue
@@ -2,11 +2,9 @@
     <v-carousel>
         <v-carousel-item v-for="forum in forums" v-bind:key="forum.forumId">
             <v-row class="fill-height" align="center" justify="center">
+                  <v-img src="https://cdn.mos.cms.futurecdn.net/jG3Csc3oK8zvPgzPn5CPMJ-1024-80.jpg.webp" max-height="200" max-width="200"></v-img>
                 <v-subtitle>
-                    {{ forum.forumTitle }}|
-                </v-subtitle>
-                <v-subtitle>
-                    <router-link  v-bind:to="{name: 'post-details' }" class="secondary--text"> Demo Here</router-link>
+                    <router-link  v-bind:to="{name: 'post-details' }" class="secondary--text">{{ forum.forumTitle }}</router-link>
                 </v-subtitle>
             </v-row>
         </v-carousel-item>    
