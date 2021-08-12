ca09f7fcd032a327c5589c06cdc003f632866068
Taylor Vandersall <taylorvandersall@gmail.com>
2021-08-12 14:16:10 -0400

    Default image

--- dotnet/Capstone/Models/Forum.cs
+++ dotnet/Capstone/Models/Forum.cs
@@ -9,6 +9,6 @@ namespace Capstone.Models
     {
         public int ForumId { get; set; }
         public string ForumTitle { get; set; }
-        public string ForumPicture { get; set; }
+        public string ForumPicture { get; set; } = "http://static1.squarespace.com/static/55ef2da9e4b03f6e1ef0cd28/t/5cddb9fb5ed3ff0001d64d24/1558034940526/Mark.jpg?format=1500w";
     }
 }
--- dotnet/Capstone/Models/Post.cs
+++ dotnet/Capstone/Models/Post.cs
@@ -15,7 +15,7 @@ namespace Capstone.Models
         public int UpvoteCounter { get; set; } = 0;
         public int DownvoteCounter { get; set; } = 0;
         public DateTime PostedDate { get; set; }
-        public string ImageURL { get; set; }
+        public string ImageURL { get; set; } = "http://static1.squarespace.com/static/55ef2da9e4b03f6e1ef0cd28/t/5cddb9fb5ed3ff0001d64d24/1558034940526/Mark.jpg?format=1500w";
     }
 
 
